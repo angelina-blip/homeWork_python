bef4e7a7e70d5274519eff74a98897b11d316322
angelina-blip <angelina835@icloud.com>
2026-01-25 20:21:35 +0300

    lesson_9

---
+++ lesson_9/db.py
@@ -0,0 +1,8 @@
+# db.py
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = "postgresql://postgres:12345@localhost:5432/QA"
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
